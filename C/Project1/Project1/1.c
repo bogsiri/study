189019ab462a2d8b4483a3a74bf50a397e5a3582
2True <ychk1205@gmail.com>
2022-04-19 00:11:59 +0900

    0419

--- C/Project1/Project1/1.c
+++ C/Project1/Project1/1.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
 
-int main(void)
+int Facrec(int n)
 {
-	return 0;
+	if (n == 1)
+		return 1;
+
+	return Facrec(n - 1) * n;
+}
+
+
+void mv(int n, char from, char mid, char to)
+{
+	if (n == 1)
+	{
+		printf("%d번째 접시를 %c에서 %c로\n", n, from, to);
+		return;
+	}
+	mv(n - 1, from, to, mid);
+	printf("%d번째 접시를 %c에서 %c로\n", n, from, to);
+	mv(n - 1, mid, from, to);
+}
+
+void main(void)
+{
+	mv(10, 'A', 'B', 'C');
+    printf("%d\n", Facrec(3));
+	return;
 }
